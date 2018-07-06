fe459c868d4bfbf76cae5811560f46f19282124b
ernie-hao <pdphiro@gmail.com>
2018-07-05 23:13:14 -0400

    Added getters/setters to user model/fixed import packages

--- src/java/webdev/repositories/UserRepository.java
+++ src/java/webdev/repositories/UserRepository.java
@@ -1,8 +1,8 @@
-package webdev.repositories;
+package java.webdev.repositories;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import webdev.models.User;
+import java.webdev.models.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 }
