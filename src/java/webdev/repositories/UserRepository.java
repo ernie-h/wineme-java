16ad7936e2ec0d7775296c1f0976e02c7a2a06b9
ernie-hao <pdphiro@gmail.com>
2018-07-06 21:22:33 -0400

    Updated folder path/package imports for java side

--- src/java/webdev/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package java.webdev.repositories;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-
-import java.webdev.models.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
