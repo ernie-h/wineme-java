979eff71732442d2c0743c6c34f5ed9cd069e183
ernie-hao <pdphiro@gmail.com>
2018-07-10 19:41:11 -0400

    Page constant refresh after submit button hit fix

--- src/main/webapp/jquery/login/login.controller.client.js
+++ src/main/webapp/jquery/login/login.controller.client.js
@@ -6,11 +6,8 @@
 
   function main() {
     $usernameFld = $('#usernameFld');
-    console.log($usernameFld);
     $passwordFld = $('#passwordFld');
     $loginBtn = $('#loginBtn');
-    console.log($loginBtn);
-
     $loginBtn.click(login);
   }
 
@@ -21,14 +18,6 @@
       userService.login($usernameStr, $passwordStr);
     } else {
       alert('Please fill in all fields.');
-    }
-  }
-
-  function loginSuccessful(response) {
-    if (response.status === 200) {
-      window.location.href = '/jquery/profile/profile.template.client.html';
-    } else {
-      alert('No user with credentials found. Please try again.');
     }
   }
 
