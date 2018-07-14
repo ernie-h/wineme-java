a7fb94c307c62d83610edd36b8cde00bf9dabe8c
ernie-hao <pdphiro@gmail.com>
2018-07-14 14:20:11 -0400

    Course db integration

--- src/main/java/com/example/myapp/repositories/CourseRepository.java
+++ src/main/java/com/example/myapp/repositories/CourseRepository.java
@@ -1,12 +1,7 @@
 package com.example.myapp.repositories;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import com.example.myapp.models.Course;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface CourseRepository extends CrudRepository<Course, Integer> {
 }
