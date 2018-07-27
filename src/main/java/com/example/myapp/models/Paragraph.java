b995e2c0a71d20289a4c5306da5306a30fa2f5bb
ernie-hao <pdphiro@gmail.com>
2018-07-27 18:35:59 -0400

    Widget Model fields fix

--- src/main/java/com/example/myapp/models/Paragraph.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapp.models;
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-
-@Entity(name = "Paragraph")
-@DiscriminatorValue("Paragraph")
-public class Paragraph extends Widget {
-
-}
